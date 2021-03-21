0c4c6136ff6a56fee60cfa181b40d74c3f565803
Lacey Walker <bluelarksong@gmail.com>
2021-03-20 23:46:50 -0500

    more SQL code with comments

--- BeginnerSQL.sql
+++ BeginnerSQL.sql
@@ -7,3 +7,9 @@ FROM books;
 -- * is short for ALL the column names in the table --
 
 -- to SELECT a specific column in a table rather than using a *, write the desired column name instead --
+SELECT first_name
+FROM clients;
+
+-- to SELECT two columns from the database in one Query, you may change the order of the columns as well by this --
+SELECT first_name, email
+FROM clients;
