8985a709cde66d17cd671f261956af7ad5448476
Lacey Walker <bluelarksong@gmail.com>
2021-03-20 23:43:37 -0500

    more comments

--- BeginnerSQL.sql
+++ BeginnerSQL.sql
@@ -4,3 +4,6 @@ FROM books;
 -- SELECT and FROM are keywords --
 -- books is the Table Name -- 
 -- the ; terminates the statement -- 
+-- * is short for ALL the column names in the table --
+
+-- to SELECT a specific column in a table rather than using a *, write the desired column name instead --
